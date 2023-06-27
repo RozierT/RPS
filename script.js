bd73828f6411df16a90f53573f700800f67708e2
Taylor Rozier <roziertaylor@gmail.com>
2023-06-26 21:24:18 -0700

    starter prompts

--- script.js
+++ script.js
@@ -1,4 +1,72 @@
-let game = prompt("Enter R, P, or S");
-if (game.toLowerCase()== "R") {
-    alert("You win")
-}
+// Variable declarations
+​
+​
+​
+// Array of options for computer to pick from
+​
+​
+// Function to playGame
+var playGame = function() {
+  // Ask user for their choice
+​
+  
+​
+  // If user pressed Cancel, immediately end function
+ 
+  
+​
+  // Convert to uppercase to make comparisons easier
+ 
+  
+​
+  // Get random index from array of options
+​
+  
+  //Assign computer's choice
+​
+​
+​
+  //Display computer's choice
+​
+  
+​
+  // If choices are the same, it's a tie.
+  if () {
+    //increment tie
+    
+​
+    //Display tie message
+    
+  // Check every possible win condition for the player for win
+  } else if (
+    
+  ) {
+​
+    //Increment win score and display message
+​
+    
+​
+  // If above conditions failed, assume player lost
+  } else {
+    //Increment loss score and display message
+​
+    
+​
+  }
+​
+  // Print stats with line breaks
+​
+  
+​
+​
+  // Ask user to play again
+​
+  
+​
+  // If user pressed OK, run the function again
+​
+  
+};
+​
+// Run the game for the first time
+playGame();
